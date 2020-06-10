26c43844f4049c1a2fe548d6b579dd546c090032
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-10 12:55:18 +0100

    Removed unnecessary parameters from App.js

--- src/App.js
+++ src/App.js
@@ -6,32 +6,19 @@ import PreviousGames from './components/PreviousGames/';
 import Player1 from './components/Player/Player1';
 import Player2 from './components/Player/Player2';
 
-const App = (
-  {
-    player1Serving,
-    handleReset,
-    previousGames
-  }
-) => {
+const App = () => {
+
   return (
-
     <React.Fragment>
-
       <Header />
-
       <div className="row mb-4">
         <Player1 />
         <Player2 />
       </div>
-
       <Winner />
-
-      <Reset handleReset={handleReset} />
-
-      <PreviousGames previousGames={previousGames} />
-
+      <Reset />
+      <PreviousGames />
     </React.Fragment >);
-
 };
 
 export default App;
