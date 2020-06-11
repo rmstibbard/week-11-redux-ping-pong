29fedd5ee233b502be2c9d2fbfee6792f73e73a3
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-11 16:12:57 +0100

    Styling for setup form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -34,44 +34,46 @@ class Form extends Component {
     let { player1Name, player2Name, winningScore, alternateEvery } = this.state;
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
+      <form onSubmit={this.handleSubmit} className="setup-form">
+        <div className="form-group game-setup">
           <label>Player 1 Name</label>
           <input
             type="text"
-            className="form-control"
+            className="form-control game-setup"
             onChange={(e) => this.handleChange(e, "player1Name")} value={player1Name}
           />
         </div>
-        <div className="form-group">
+        <div className="form-group game-setup">
           <label>Player 2 Name</label>
           <input
             type="text"
-            className="form-control"
+            className="form-control game-setup"
             onChange={(e) => this.handleChange(e, "player2Name")} value={player2Name}
           />
         </div>
-        <div className="form-group">
+        <div className="form-group game-setup">
           <label>Winning Score</label>
           <input
             type="number"
             min="1"
             max="21"
-            className="form-control"
+            className="form-control game-setup"
             onChange={(e) => this.handleChange(e, "winningScore")} value={winningScore}
           />
         </div>
-        <div className="form-group">
+        <div className="form-group game-setup">
           <label>Change Server Interval</label>
           <input
-            className="form-control"
+            className="form-control game-setup"
             type="number"
             min="1"
             max="10"
             onChange={(e) => this.handleChange(e, "alternateEvery")} value={alternateEvery}
           />
         </div>
-        <button className="btn btn-primary">Save</button>
+        <div className="game-setup">
+          <button className="btn btn-primary game-setup">Save</button>
+        </div>
       </form>
     );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => (
-  <header className="jumbotron mt-4 mb-0">
+  <header className="jumbotron mt-4 mb-0 header">
     <h1>PongPing</h1>
   </header>
 )
